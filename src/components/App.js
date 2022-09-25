c068f83b6c66e93b66b21ca6515f11d837c191a1
AkashMore <akashmoref121438@gmail.com>
2022-09-25 07:30:35 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test = "project-name">project name<h1/>
-      <h2 data-ns-test = "project-description"><h2>
+      <h1 data-ns-test = "project-name">Project name<h1/>
+      <h2 data-ns-test = "project-description">Project description.<h2>
     </div>
   )
 }
